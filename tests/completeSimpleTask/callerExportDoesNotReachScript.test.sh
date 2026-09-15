8203dba03d7eb361db7753c0dd4984faf18e71fa
patrick <patrick@lessonswithpatrick.com>
2026-09-15 12:24:13 -0700

    fixed problem with edits changing file permissions

--- tests/completeSimpleTask/callerExportDoesNotReachScript.test.sh
+++ tests/completeSimpleTask/callerExportDoesNotReachScript.test.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-# Exported PID / PROMPT_COUNTER / REPLACE_TEMP_FILE / TEMP_FOLDER
-# from the caller do not reach the script.
+# Exported PID / PROMPT_COUNTER / TEMP_FOLDER from the caller do not
+# reach the script.
 
 set -u
 . "$TEST_UTILS_FILE"
@@ -12,7 +12,6 @@ add_model_reply 1 <<'REPLY'
 # script
 printf 'PID=%s\n' "${PID-unset}"
 printf 'PROMPT_COUNTER=%s\n' "${PROMPT_COUNTER-unset}"
-printf 'REPLACE_TEMP_FILE=%s\n' "${REPLACE_TEMP_FILE-unset}"
 printf 'TEMP_FOLDER=%s\n' "${TEMP_FOLDER-unset}"
 REPLY
 
@@ -22,12 +21,10 @@ REPLY
 
 export PID=from-caller
 export PROMPT_COUNTER=99
-export REPLACE_TEMP_FILE=/from-caller
 export TEMP_FOLDER=/from-caller
 run_ssa_task print whether private names leaked
 expect_exit 0
 expect_stdout_has 'PID=unset'
 expect_stdout_has 'PROMPT_COUNTER=unset'
-expect_stdout_has 'REPLACE_TEMP_FILE=unset'
 expect_stdout_has 'TEMP_FOLDER=unset'
 expect_stderr_has 'done: after 2 model prompts'
